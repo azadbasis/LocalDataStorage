15fc81f6c068f571b769a4419d3d08464fa71fab
azadbasis <azadbasis@gmail.com>
2018-12-05 13:11:26 +0600

    Display Static Data(Collections)

--- app/src/androidTest/java/me/azhar/data/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/me/azhar/data/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("me.azhar.data", appContext.getPackageName());
+        assertEquals("me.azhar.modeldata", appContext.getPackageName());
     }
 }
--- app/src/main/java/me/azhar/data/MainActivity.java
+++ app/src/main/java/me/azhar/data/MainActivity.java
@@ -4,24 +4,41 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import me.azhar.data.model.DataItem;
+import me.azhar.data.sample.SampleDataProvider;
 
 
 public class MainActivity extends AppCompatActivity {
 
-        TextView tvOut;
+    TextView tvOut;
+    List<DataItem> dataItemList = SampleDataProvider.dataItemList;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            DataItem item =
+            /*DataItem item =
                     new DataItem(null, "My menu item", "a category",
                             "a description", 1, 9.95, "apple_pie.jpg");
+*/
+        tvOut = (TextView) findViewById(R.id.out);
+        tvOut.setText("");
 
-            tvOut = (TextView) findViewById(R.id.out);
-            tvOut.setText(item.toString());
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getItemName().compareTo(o2.getItemName());
+            }
+        });
 
+        for (DataItem item : dataItemList) {
+            tvOut.append(item.getItemName() + "\n");
         }
+
+    }
 }
