40265d96f65a754da7a9711e770b503b1d4f83b6
azadbasis <azadbasis@gmail.com>
2018-12-05 13:50:03 +0600

    Display Static Data(Array Adapter)

--- app/src/main/java/me/azhar/data/MainActivity.java
+++ app/src/main/java/me/azhar/data/MainActivity.java
@@ -2,13 +2,13 @@ package me.azhar.data;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import me.azhar.data.adapter.DataItemAdapter;
 import me.azhar.data.model.DataItem;
 import me.azhar.data.sample.SampleDataProvider;
 
@@ -16,22 +16,23 @@ import me.azhar.data.sample.SampleDataProvider;
 public class MainActivity extends AppCompatActivity {
 
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
-    List<String> stringList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getItemName().compareTo(o2.getItemName());
+            }
+        });
+        DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
 
-        for (DataItem item : dataItemList)
-            stringList.add(item.getItemName());
-        Collections.sort(stringList);
 
-
-        ListView listView=(ListView)findViewById(android.R.id.list);
-        ArrayAdapter<String> stringArrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,stringList);
-        listView.setAdapter(stringArrayAdapter);
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
 
 
     }
