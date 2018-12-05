0d3969ff45f687e12918680b01c18baddc99c054
azadbasis <azadbasis@gmail.com>
2018-12-05 15:48:16 +0600

    Store Data in SharedPreferences(PreferencesJava)

--- app/src/main/java/me/azhar/data/MainActivity.java
+++ app/src/main/java/me/azhar/data/MainActivity.java
@@ -1,21 +1,31 @@
 package me.azhar.data;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
+
+
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import me.azhar.data.R;
+import me.azhar.data.SigninActivity;
 import me.azhar.data.adapter.DataItemAdapter;
 import me.azhar.data.model.DataItem;
 import me.azhar.data.sample.SampleDataProvider;
 
-
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SIGNIN_REQUEST = 1001;
+    public static final String MY_GLOBAL_PREFS = "my_global_prefs";
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
 
     @Override
@@ -29,12 +39,43 @@ public class MainActivity extends AppCompatActivity {
                 return o1.getItemName().compareTo(o2.getItemName());
             }
         });
-        DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
 
+        DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvItems);
         recyclerView.setAdapter(adapter);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_signin:
+                Intent intent = new Intent(this, SigninActivity.class);
+                startActivityForResult(intent, SIGNIN_REQUEST);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == SIGNIN_REQUEST) {
+            String email = data.getStringExtra(SigninActivity.EMAIL_KEY);
+            Toast.makeText(this, "You signed in as " + email, Toast.LENGTH_SHORT).show();
+
+            SharedPreferences.Editor editor =
+                    getSharedPreferences(MY_GLOBAL_PREFS, MODE_PRIVATE).edit();
+            editor.putString(SigninActivity.EMAIL_KEY, email);
+            editor.apply();
+
+        }
 
     }
 }
