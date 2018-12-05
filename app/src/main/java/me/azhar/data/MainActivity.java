537a3553fd4930b4109b0b6fe4c494e7658f65b1
azadbasis <azadbasis@gmail.com>
2018-12-05 16:41:41 +0600

    Store Data in Text Files(InternalStorage)

--- app/src/main/java/me/azhar/data/MainActivity.java
+++ app/src/main/java/me/azhar/data/MainActivity.java
@@ -1,89 +1,64 @@
 package me.azhar.data;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-import me.azhar.data.adapter.DataItemAdapter;
-import me.azhar.data.model.DataItem;
-import me.azhar.data.sample.SampleDataProvider;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int SIGNIN_REQUEST = 1001;
-    public static final String MY_GLOBAL_PREFS = "my_global_prefs";
-    List<DataItem> dataItemList = SampleDataProvider.dataItemList;
+
+    TextView output;
+    //public static final String FILE_NAME = "lorem_ipsum.txt";
+    public static final String FILE_NAME = "azhar.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Collections.sort(dataItemList, new Comparator<DataItem>() {
-            @Override
-            public int compare(DataItem o1, DataItem o2) {
-                return o1.getItemName().compareTo(o2.getItemName());
+        output = (TextView) findViewById(R.id.outputText);
+        output.setText(getString(R.string.ready_to_code));
+    }
+
+    public void onCreateButtonClick(View view) {
+
+        String string = getString(R.string.lorem_ipsum);
+        FileOutputStream fileOutputStream = null;
+        File file = new File(FILE_NAME);
+
+        try {
+            fileOutputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fileOutputStream.write(string.getBytes());
+            Toast.makeText(this, "File Written: " + FILE_NAME, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();//heeeloow
             }
-        });
-
-        DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
-
-        SharedPreferences settings= PreferenceManager.getDefaultSharedPreferences(this);
-        boolean grid=settings.getBoolean(getString(R.string.pref_display_grid),false);
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvItems);
-        if(grid){
-            recyclerView.setLayoutManager(new GridLayoutManager(this,3));
         }
-        recyclerView.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void onReadButtonClick(View view) {
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_signin:
-                Intent intent = new Intent(this, SigninActivity.class);
-                startActivityForResult(intent, SIGNIN_REQUEST);
-                return true;
-            case R.id.action_settings:
-                Intent settingIntent = new Intent(this, PrefsActivity.class);
-                startActivity(settingIntent);
-                return true;
+    public void onDeleteButtonClick(View view) {
+
+        File file=new File(getFilesDir(),FILE_NAME);
+        if(file.exists()){
+            deleteFile(FILE_NAME);
+            Toast.makeText(this, "File deleted successfully", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "File does not exist", Toast.LENGTH_SHORT).show();
         }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK && requestCode == SIGNIN_REQUEST) {
-            String email = data.getStringExtra(SigninActivity.EMAIL_KEY);
-            Toast.makeText(this, "You signed in as " + email, Toast.LENGTH_SHORT).show();
-
-            SharedPreferences.Editor editor =
-                    getSharedPreferences(MY_GLOBAL_PREFS, MODE_PRIVATE).edit();
-            editor.putString(SigninActivity.EMAIL_KEY, email);
-            editor.apply();
-
-        }
-
     }
 }
