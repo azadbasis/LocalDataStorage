5ab3211dbee3d223d79739ce0c81b05fb482377c
azadbasis <azadbasis@gmail.com>
2018-12-05 12:59:58 +0600

    Android Local Data Storage

---
+++ app/src/main/java/me/azhar/data/MainActivity.java
@@ -0,0 +1,27 @@
+package me.azhar.data;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import me.azhar.data.model.DataItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+        TextView tvOut;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            DataItem item =
+                    new DataItem(null, "My menu item", "a category",
+                            "a description", 1, 9.95, "apple_pie.jpg");
+
+            tvOut = (TextView) findViewById(R.id.out);
+            tvOut.setText(item.toString());
+
+        }
+}
